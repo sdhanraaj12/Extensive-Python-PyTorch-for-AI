9d869c644b663e364e641135eda8093aff6f4870
sdhanraaj12 <sdhanraaj12@gmail.com>
2021-06-26 02:15:53 +0530

    Update test_session7.py

--- Session7/test_session7.py
+++ Session7/test_session7.py
@@ -1,246 +1,200 @@
-import session7
 import pytest
-import os 
+import random
+import session7
+import os
 import inspect
-import re 
+import re
+import math
+from random import randint
+from session7 import *
 
-README_CONTENT_CHECK_FOR = [
-    'check_docstring',
-    'generate_fibanoci',
-    'function_counter',
-    'function_counter_mod'
-]
+# ------------------------ General Test Cases -----------------------------------
 
-### Readme test
+README_CONTENT_CHECK_FOR = ['Fibonacci', 'docstring', 'count', 'closure']
 
-def test_session7_readme_exists():
-    """ Checks if readme.md file exists    """    
+def test_readme_exists():
     assert os.path.isfile("README.md"), "README.md file missing!"
 
-def test_session7_readme_100_words():
-    """ Checks if Readme contains atleast 500 words """    
-    readme = open("README.md", "r")
+
+def test_readme_contents():
+    readme = open("README.md", "r", encoding="utf-8")
     readme_words = readme.read().split()
     readme.close()
-    assert len(readme_words) >= 100, "Make your README.md file interesting! Add atleast 500 words"
+    readme_words = [word for line in open('README.md', 'r') for word in line.split()]
+    assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
 
 
-def test_session7_readme_proper_description():
-    """ Checks if important contents are being convered in readme    """    
+def test_readme_proper_description():
     READMELOOKSGOOD = True
     f = open("README.md", "r", encoding="utf-8")
     content = f.read()
     f.close()
     for c in README_CONTENT_CHECK_FOR:
-        if c not in content:            
+        if c not in content:
             READMELOOKSGOOD = False
             pass
     assert READMELOOKSGOOD == True, "You have not described all the functions/class well in your README.md file"
 
 
-def test_session7_readme_file_for_more_than_10_hashes():
-    """ Checks if we have done proper formating  """    
+def test_readme_file_for_formatting():
     f = open("README.md", "r", encoding="utf-8")
     content = f.read()
     f.close()
-    assert content.count("#") >= 10
+    assert content.count("#") >= 10, "Make the format look good"
 
-def test_session7_function_name_had_cap_letter():
-    """ Checks if all function in session5 has used any captial letters    """    
+
+def test_indentations():
+    ''' Returns pass if used four spaces for each level of syntactically \
+    significant indenting.'''
+    lines = inspect.getsource(session7)
+    spaces = re.findall('\n +.', lines)
+    for space in spaces:
+        assert len(space) % 4 == 2, "Your script contains misplaced indentations"
+        assert len(re.sub(r'[^ ]', '', space)) % 4 == 0, "Your code indentation does not follow PEP8 guidelines" 
+
+def test_function_name_had_cap_letter():
     functions = inspect.getmembers(session7, inspect.isfunction)
     for function in functions:
         assert len(re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
 
-## Tast 1 test
 
 
 
-# doc strings,  input value , check if 50is a free variable
+def test_function_doc_string():
+    '''
+    Test case to check whether the functions have docstrings or not.
+    '''
+    functions = inspect.getmembers(session7, inspect.isfunction)
+    for function in functions:
+        assert function[1].__doc__, "You have not documented the code!"
 
-def test_check_docstring_doc_string():
-    """ Check if doc string is more than 50 in length"""
-    assert len(session7.check_docstring.__doc__) > 50, "Please improve your length of doc strings" 
+def test_function_annotations():
+    '''
+    Test case to check whether the functions have annotations or not.
+    '''
+    functions = inspect.getmembers(session7, inspect.isfunction)
+    for function in functions:
+        assert function[1].__annotations__, "You have not annotated the code!"
 
-def test_input_values():
-    """ Checks if Type error is raised for string input and non function input"""
-    with pytest.raises(TypeError):
-        session7.check_docstring('fifty')
-    with pytest.raises(TypeError):        
-        inp = session7.check_docstring(50)
-        inp(34)
+# ------------------------------------ Doc String Check function Test Case --------------------------
 
-def test_check_docstring_closure():
-    """ Checks if function has closure """
-    inp = session7.check_docstring(50)
-    assert len(inp.__closure__) >= 1
+# Dummy function to test
+def test_func_true():
+    """
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    Function to check 50 characters doc string
+    """
+    return
+# Dummy function to test
+def test_func_false():
+    """
+    Less Doc
+    """
+    return
 
-def test_free_variable():
-    """ Checks if there is a free variable """
-    assert len(session7.check_docstring(50).__code__.co_freevars) == 1, "There should be 1 free variable"
-
-def test_check_docstring_funcitonality():
-    """ Checks if a Closure function has doc string greater than 50 """
-    def add(a,b):
-        '''
-        This function is one and one and it adds
-        This function is one and one and it adds
-        a: input of first number to be added
-        b: input of second number to be added
-        return total: sum of a+b
-        '''
-        total = a+b
-        return total
-    docStringLengthCheck = session7.check_docstring(50)
-    assert docStringLengthCheck(add) == True, "Please check your doc string functionality"
-
-def test_generate_fibanoci_closure():
-    """ Checks if function has closure """
-    fibGenerate = session7.generate_fibanoci()
-    assert len(fibGenerate.__closure__) >= 1, "You need to write a closure"
-
-def test_generate_fibanoci_functionality():
-    """ Checks if fibanoci number is generated next to next """
-    fibGenerate = session7.generate_fibanoci()
-    assert fibGenerate() == 0, "Please check you closure functionality"
-    assert fibGenerate() == 1, "Please check you closure functionality"
-    assert fibGenerate() == 1, "Please check you closure functionality"
-    assert fibGenerate() == 2, "Please check you closure functionality"
-    assert fibGenerate() == 3, "Please check you closure functionality"
-    assert fibGenerate() == 5, "Please check you closure functionality"
-
-## Task 3
-
-def test_function_counter_closure_presence():
-    """ Checks if function has closure """
-    funcCount = session7.function_counter()    
-    assert len(funcCount.__closure__) >= 1, "You need to write a closure"
+def test_check_dock_true():
+    dc = check_doc_outer_fn()
+    assert dc(test_func_true) == True, "Something wrong with the check_doc_fn function."
 
 
-def test_function_counter_functionality():
-    """ Checks if function counter has its functionlality """
-    funcCount = session7.function_counter()
-    def mult(a, b, c):
-        return a * b * c 
+def test_check_dock_false():
+    dc = check_doc_outer_fn()
+    assert dc(test_func_false) == False, "Something wrong with the check_doc_fn function."
+
+
+# ------------------------------------ Fibonacci Test Case --------------------------
+
+def test_fibonacci():
+    fib = fibonacci()
+    assert fib() == 0, "Something wrong with the fibonacci function."
+    assert fib() == 1, "Something wrong with the fibonacci function."
+    assert fib() == 1, "Something wrong with the fibonacci function."
+    assert fib() == 2, "Something wrong with the fibonacci function."
+   
+# ------------------------------------ Counter Test Case  --------------------------
+def test_func_count():
+
     def add(a, b):
-        return a + b
-    def div(a, b):
-        return a/ b
-    funcCount(add, 1, 2)
-    funcCount(mult, 1, 2, 3)
-    funcCount(add, 1, 2)
-    funcCount(mult, 1, 2, 3)
-    funcCount(div, 4, 2)
-    funcCount(div, 4, 2)
-    assert session7.function_call_counter == {'add' : 2,'mult' : 2, 'div': 2}, "Please check functionality"
-    funcCount(mult, 1, 2, 3)
-    assert session7.function_call_counter == {'add' : 2,'mult' : 3, 'div': 2}, "Please check functionality"
+        return a+b
+    def mul(a, b):
+        return a*b
+    def randomf():
+        pass
+    fc = func_counter()
+    for i in range(5):
+        fc(add, 2, 5)
+    for i in range(3):
+        fc(mul, 2, 5)
+    for i in range(10):
+        fc(randomf)
+   
+    
+    assert session7.call_count['add'] == 5, "Something wrong with the func_count function."
+    assert session7.call_count['mul'] == 3, "Something wrong with the func_count function."
+    assert session7.call_count['randomf'] == 10, "Something wrong with the func_count function."
+
+def test_func_count_check_error():
+    with pytest.raises(TypeError):
+        fc = func_counter()
+        fc("Random Checks")
+
+# ------------------------------------ Different Dictonary Test Case --------------------------
+
+def test_func_count_with_dict_outer():
+
+    def add(a, b):
+        return a+b
+    def mul(a, b):
+        return a*b
+    def randomf():
+        pass
+
+    dict_1 = dict()
+    fc = func_count_with_dict_outer(dict_1)
+
+    a, b, c = randint(1, 10),randint(1, 10),randint(1, 10)
+
+    for i in range(a):
+        fc(add, 2, 5)
+    for i in range(b):
+        fc(mul, 2, 5)
+    for i in range(c):
+        fc(randomf)
+
+    assert dict_1['add'] == a, "Something wrong with the func_count function."
+    assert dict_1['mul'] == b, "Something wrong with the func_count function."
+    assert dict_1['randomf'] == c, "Something wrong with the func_count function."
+  
+    dict_2 = dict()
+    fc_new = func_count_with_dict_outer(dict_2)
+
+    a, b, c = randint(1, 10),randint(1, 10),randint(1, 10)
+
+    for i in range(a):
+        fc_new(add, 2, 5)
+    for i in range(b):
+        fc_new(mul, 2, 5)
+    for i in range(c):
+        fc_new(randomf)
     
 
-def test_function_counter_dictionary_assurance():
-    """ Checks if we change global variable "function_call_counter" and if its changing 
-    original counter """
-    funcCount = session7.function_counter()
-    def mult(a, b, c):
-        return a * b * c 
-    def add(a, b):
-        return a + b
-    def div(a, b):
-        return a/ b
-    session7.function_call_counter = {'add': 4}
-    funcCount(add, 1, 2)
-    funcCount(mult, 1, 2, 3)
-    funcCount(add, 1, 2)
-    funcCount(mult, 1, 2, 3)
-    funcCount(div, 4, 2)
-    funcCount(div, 4, 2)
-    
-    assert session7.function_call_counter == {'add' : 2,'mult' : 2, 'div': 2}, "Please check functionality"
-    
-def test_function_counter_input_check():
-    """ Checks if its taking correct input """
-    funcCount = session7.function_counter()    
-    def add(a, b):
-        return a + b    
+    assert dict_2['add'] == a, "Something wrong with the func_count function."
+    assert dict_2['mul'] == b, "Something wrong with the func_count function."
+    assert dict_2['randomf'] == c, "Something wrong with the func_count function."
+
+
+def test_func_count_with_dict_outer_check_error():
     with pytest.raises(TypeError):
-        funcCount(4, 1, 2)
-
-def test_function_counter_doc_string_presence():
-    """ Checks if doc strings are present in function """
-    assert len(session7.function_counter.__doc__) > 10, "Please improve your length of doc strings" 
-
-
-def test_function_counter_annotations():
-    """ Checks if annotations are  present in function """
-    assert len(session7.function_counter.__annotations__) > 0, "Please write annotations"
-
-## Test next 6 
-
-
-
-
-def test_function_counter_mod_doc_string_presence():
-    """ Checks if doc strings are present in function """
-    assert len(session7.function_counter_mod.__doc__) > 10, "Please improve your length of doc strings" 
-
-
-def test_function_counter_mod_annotations():
-    """ Checks if annotations are  present in function """
-    assert len(session7.function_counter_mod.__annotations__) > 0, "Please write annotations"
-
-def test_generate_function_input_check():
-    """ Checks if we are providing a function to closure """
-    dict1  = dict()
-    funcCount = session7.function_counter_mod(dict1)    
-    def add(a, b):
-        return a + b    
-    with pytest.raises(TypeError):
-        funcCount(4, 1, 2)
-
-def test_function_counter_mod_functionality():
-    """ Checks if properfly the function is taking place """
-    function_call_counter_user = {'add':45,'mult':3}
-    funcCount = session7.function_counter_mod(function_call_counter_user)
-    def mult(a, b, c):
-        return a * b * c 
-    def add(a, b):
-        return a + b
-    def div(a, b):
-        return a/ b
-    funcCount(add, 1, 2)
-    funcCount(mult, 1, 2, 3)
-    funcCount(add, 1, 2)
-    funcCount(mult, 1, 2, 3)
-    funcCount(div, 4, 2)
-    funcCount(div, 4, 2)    
-    assert function_call_counter_user == {'add' : 47,'mult' : 5, 'div': 2}, "Please check functionality"
-    funcCount(mult, 1, 2, 3)
-    assert function_call_counter_user == {'add' : 47,'mult' : 6, 'div': 2}, "Please check functionality"
-
-def test_function_counter_mod_dict_assurance():
-    """ Dictionary is modified at middle and checks if it rreturns it """
-    function_call_counter_user = {}
-    funcCount = session7.function_counter_mod(function_call_counter_user)
-    def mult(a, b, c):
-        return a * b * c 
-    def add(a, b):
-        return a + b
-    def div(a, b):
-        return a/ b
-    funcCount(add, 1, 2)
-    funcCount(mult, 1, 2, 3)
-    funcCount(add, 1, 2)
-    function_call_counter_user = {'add':45,'mult':3}
-    funcCount(mult, 1, 2, 3)
-    funcCount(div, 4, 2)
-    funcCount(div, 4, 2)        
-    assert function_call_counter_user == {'add':45, 'mult' : 3}, "Please provide user defined dict properly"
+        fc = func_count_with_dict_outer()
     
-def test_function_counter_mod_input_check_dict():
-    """ Checks if the input is a dictionary to function """
-    with pytest.raises(TypeError):
-        session7.function_counter_mod(45)    
-
-
-
-
-
+  
